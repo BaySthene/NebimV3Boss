81863bb2a764e1fb0d24888ec6eba4a33378ede1
baysthene <muhammet.keskin@ogr.deu.edu.tr>
2024-10-09 09:38:46 +0300

    yükle

--- app/navigators/AppNavigator.tsx
+++ app/navigators/AppNavigator.tsx
@@ -1,9 +1,3 @@
-/**
- * The app navigator (formerly "AppNavigator" and "MainNavigator") is used for the primary
- * navigation flows of your app.
- * Generally speaking, it will contain an auth flow (registration, login, forgot password)
- * and a "main" flow which the user will use once logged in.
- */
 import {
   DarkTheme,
   DefaultTheme,
@@ -20,21 +14,6 @@ import { navigationRef, useBackButtonHandler } from "./navigationUtilities"
 import { colors } from "app/theme"
 import { DashboardParamList } from "app/screens"
 
-
-
-/**
- * This type allows TypeScript to know what routes are defined in this navigator
- * as well as what properties (if any) they might take when navigating to them.
- *
- * If no params are allowed, pass through `undefined`. Generally speaking, we
- * recommend using your MobX-State-Tree store(s) to keep application state
- * rather than passing state through navigation params.
- *
- * For more information, see this documentation:
- *   https://reactnavigation.org/docs/params/
- *   https://reactnavigation.org/docs/typescript#type-checking-the-navigator
- *   https://reactnavigation.org/docs/typescript/#organizing-types
- */
 export type LoginPasswordScreenParams = {
   LoginPassword: {
     isVisible: boolean,
@@ -51,22 +30,13 @@ export type AppStackParamList = {
   RegisterParams: undefined,
   Dashboard: DashboardParamList
   InsideNavigator: undefined,
-  // 🔥 Your screens go here
-  // IGNITE_GENERATOR_ANCHOR_APP_STACK_PARAM_LIST
 }
-
-/**
- * This is a list of all the route names that will exit the app if the back button
- * is pressed while in that screen. Only affects Android.
- */
 const exitRoutes = Config.exitRoutes
 
 export type AppStackScreenProps<T extends keyof AppStackParamList> = NativeStackScreenProps<
   AppStackParamList,
   T
 >
-
-// Documentation: https://reactnavigation.org/docs/stack-navigator/
 const Stack = createNativeStackNavigator<AppStackParamList>()
 
 const AppStack = observer(function AppStack() {
--- app/screens/Auth/RegisterParamsScreen.tsx
+++ app/screens/Auth/RegisterParamsScreen.tsx
@@ -2,17 +2,23 @@ import { AppStackScreenProps } from "app/navigators"
 import React, { ComponentType, FC, useMemo, useRef, useState } from "react"
 import { observer } from "mobx-react-lite"
 import { Button, Icon, Screen, Text, TextField, TextFieldAccessoryProps } from "app/components"
-import { ImageBackground, Pressable, TextInput, TextStyle, View, ViewStyle } from "react-native"
+import { ImageBackground, Pressable, ScrollView, TextInput, TextStyle, View, ViewStyle } from "react-native"
 import { colors, spacing } from "app/theme"
 import Animated, { FadeInLeft, FadeOutLeft } from "react-native-reanimated"
 import { changeLanguage } from "app/i18n"
+import { useStores } from "app/models"
 interface RegisterParamsProps extends AppStackScreenProps<"RegisterParams"> {}
 
 export const RegisterParamsScreen: FC<RegisterParamsProps> = observer(function RegisterParamsScreen(_props) {
-  const authVKNInput = useRef<TextInput>(null)
+  const {
+    authenticationStore: { validationError, isRecorded, register },
+  } = useStores()
+  const authFirstNameInput = useRef<TextInput>(null)
+  const authLastNameInput = useRef<TextInput>(null)
   const authPasswordInput = useRef<TextInput>(null)
   const  [ language, setLanguage ] = useState('tr');
-  const [ authVKN, setAuthVKN ] = useState('');
+  const [ authFirstName, setAuthFirstName ] = useState(isRecorded ? register.firstName : '');
+  const [ authLastName, setAuthLastName ] = useState(isRecorded ? register.lastName : '');
   const [authPassword, setAuthPassword] = useState("")
   const [isAuthPasswordHidden, setIsAuthPasswordHidden] = useState(true)
   const changeLanguageHandle = async () => {
@@ -41,83 +47,67 @@ export const RegisterParamsScreen: FC<RegisterParamsProps> = observer(function R
   )
   return (
     <Screen
-      preset="fixed"
+      preset="scroll"
       contentContainerStyle={$screenContentContainer}
       safeAreaEdges={["top"]}
     >
-      <ImageBackground style={$imageBackgroundContainer} width={1000} source={{uri: 'https://nebim.istanbul/assets/media/auth/bg10.jpeg'}} resizeMode="cover">
-        <View style={{ alignItems: 'flex-end', paddingVertical: spacing.md, paddingHorizontal: spacing.lg, paddingBottom: spacing.xxxl, paddingRight: -spacing.xl}}>
-          <Pressable onPress={() => changeLanguageHandle()}>
-            <Text tx='loginScreen.language' />
-          </Pressable>
-        </View>
-        <Animated.View entering={FadeInLeft.duration(400).delay(500)} exiting={FadeOutLeft.duration(400).delay(500)} >
-          <Text testID="login-heading" preset="heading" text="Nebim V3 Portal" style={$brandHeadingText} />
-          <TextField
-            style={$textField}
-            ref={authVKNInput}
-            value={authVKN}
-            onChangeText={setAuthVKN}
-            containerStyle={$textFieldContainer}
-            autoCapitalize="none"
-            autoCorrect
-            labelTx="loginScreen.firstNameLabel"
-            placeholderTx="loginScreen.firstNamePlaceholder"
-            onSubmitEditing={() => console.log('asdad')}
-            inputWrapperStyle={{ alignItems: 'center'}}
-            autoFocus
-          />
-          <TextField
-            style={$textField}
-            ref={authVKNInput}
-            value={authVKN}
-            onChangeText={setAuthVKN}
-            containerStyle={$textFieldContainer}
-            autoCapitalize="none"
-            autoCorrect
-            labelTx="loginScreen.lastNameLabel"
-            placeholderTx="loginScreen.lastNamePlaceholder"
-            onSubmitEditing={() => console.log('asdad')}
-            inputWrapperStyle={{ alignItems: 'center'}}
-            autoFocus
-          />
-          <TextField
-            style={$textField}
-            ref={authPasswordInput}
-            value={authPassword}
-            onChangeText={setAuthPassword}
-            containerStyle={$textFieldContainer}
-            autoCapitalize="none"
-            autoComplete="password"
-            autoCorrect={false}
-            secureTextEntry={isAuthPasswordHidden}
-            labelTx="loginScreen.passwordFieldLabel"
-            placeholderTx="loginScreen.passwordFieldPlaceholder"
-            RightAccessory={PasswordRightAccessory}
-            inputWrapperStyle={{ alignItems: 'center'}}
-            autoFocus
-          />
-          <TextField
-            style={$textField}
-            ref={authPasswordInput}
-            value={authPassword}
-            onChangeText={setAuthPassword}
-            containerStyle={$textFieldContainer}
-            autoCapitalize="none"
-            autoComplete="password"
-            autoCorrect={false}
-            secureTextEntry={isAuthPasswordHidden}
-            labelTx="loginScreen.passwordFieldLabel"
-            placeholderTx="loginScreen.passwordFieldPlaceholder"
-            RightAccessory={PasswordRightAccessory}
-            inputWrapperStyle={{ alignItems: 'center'}}
-            autoFocus
-          />
-          <Button style={{ marginVertical: spacing.md }} preset="reversed" tx="loginScreen.tapToSignIn"  />
+     <ScrollView style={$imageBackgroundContainer}>
+         <View style={{ alignItems: 'flex-end', paddingVertical: spacing.md, paddingHorizontal: spacing.lg, paddingBottom: spacing.xxxl, paddingRight: -spacing.xl}}>
+           <Pressable onPress={() => changeLanguageHandle()}>
+             <Text tx='loginScreen.language' />
+           </Pressable>
+         </View>
+         <Animated.View entering={FadeInLeft.duration(400).delay(500)} exiting={FadeOutLeft.duration(400).delay(500)} >
+           <Text testID="login-heading" preset="heading" tx="base.name" style={$brandHeadingText} />
+           <TextField
+             style={$textField}
+             ref={authFirstNameInput}
+             value={authFirstName}
+             onChangeText={setAuthFirstName}
+             containerStyle={$textFieldContainer}
+             autoCapitalize="none"
+             autoCorrect
+             labelTx="loginScreen.firstNameLabel"
+             placeholderTx="loginScreen.firstNamePlaceholder"
+             onSubmitEditing={() => authFirstNameInput.current && authLastNameInput.current.focus()}
+             inputWrapperStyle={{ alignItems: 'center'}}
+             autoFocus
+           />
+           <TextField
+             style={$textField}
+             ref={authLastNameInput}
+             value={authLastName}
+             onChangeText={setAuthLastName}
+             containerStyle={$textFieldContainer}
+             autoCapitalize="none"
+             autoCorrect
+             labelTx="loginScreen.lastNameLabel"
+             placeholderTx="loginScreen.lastNamePlaceholder"
+             onSubmitEditing={() => authLastNameInput.current && authPasswordInput.current.focus()}
+             inputWrapperStyle={{ alignItems: 'center'}}
+             autoFocus
+           />
+           <TextField
+             style={$textField}
+             ref={authPasswordInput}
+             value={authPassword}
+             onChangeText={setAuthPassword}
+             containerStyle={$textFieldContainer}
+             autoCapitalize="none"
+             autoComplete="password"
+             autoCorrect={false}
+             secureTextEntry={isAuthPasswordHidden}
+             labelTx="loginScreen.passwordFieldLabel"
+             placeholderTx="loginScreen.passwordFieldPlaceholder"
+             RightAccessory={PasswordRightAccessory}
+             inputWrapperStyle={{ alignItems: 'center'}}
+             autoFocus
+           />
+           <Button style={{ marginVertical: spacing.md }} preset="reversed" tx="loginScreen.tapToSignIn"  />
 
-        </Animated.View>
+         </Animated.View>
 
-      </ImageBackground>
+     </ScrollView>
     </Screen>
   )
 });
